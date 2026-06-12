4fe78d8ca6f41eb1baa019f04c5bc0814324705d
Ashish Winiston D <ashishwiniston11@gmail.com>
2026-06-13 00:29:26 +0530

    First Commit

--- project/src/pages/LoginPage.tsx
+++ project/src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+git initimport { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { Brain, Mail, Lock, Eye, EyeOff, ChevronRight, AlertCircle } from 'lucide-react';
